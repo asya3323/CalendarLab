1c7133980c6851b2af4b938439070e5502d7ffab
asya3323 <asya.master3323@gmail.com>
2024-01-04 17:31:41 +0300

    Initial commit

---
+++ CalendarLab/Presenter/DateData.cs
@@ -0,0 +1,22 @@
+﻿namespace CalendarLab.Presenter;
+
+public class DateData
+{
+    public int year { get; set; }
+    public int month { get; set; }
+    public int day { get; set; }
+    public bool IsLeapYear { get; set; }
+
+    public DateData(DateTime date)
+    {
+        this.year = date.Year;
+        this.month = date.Month;
+        this.day = date.Day;
+        IsLeapYear = DateTime.IsLeapYear(year);
+    }
+
+    public override string ToString()
+    {
+        return $"{day}.{month}.{year}";
+    }
+}
